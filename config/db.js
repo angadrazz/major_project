6dd098d15927dbc981c29d43217b9b16bf042e59
angadrazz <angadraj9534@gmail.com>
2023-05-26 11:42:02 +0530

    change in db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb://localhost:27017/Ecommerce", {
+  mongoose.connect("mongodb://localhost:27017/ecommerce", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
